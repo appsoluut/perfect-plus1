36f438a67a4dc851068317168de7e07602441a91
Paul Hameteman <paul@hameteman.com>
2024-08-23 21:05:53 +0200

    Splashscreen

--- tiled/tiles.tsx
+++ tiled/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="16" tileheight="16" tilecount="5" columns="5">
- <image source="../main/tiles.png" width="80" height="16"/>
+ <image source="../game/tiles.png" width="80" height="16"/>
 </tileset>
