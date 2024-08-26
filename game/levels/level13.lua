c8b5eda2b79aaafcd385d7103f4915d5a6b3aedb
Paul Hameteman <paul@hameteman.com>
2024-08-26 16:27:25 +0200

    Added teleport level

---
+++ game/levels/level13.lua
@@ -0,0 +1,117 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "left-up",
+  width = 10,
+  height = 10,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 4,
+  nextobjectid = 1,
+  properties = {
+    ["moves"] = 7,
+    ["name"] = "TELEPORTING IN THREE"
+  },
+  tilesets = {
+    {
+      name = "tiles",
+      firstgid = 1,
+      filename = "../../tiled/tiles.tsx"
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      id = 1,
+      name = "floor",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 3, 3, 0, 0, 0, 0,
+        0, 0, 0, 0, 3, 3, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 3, 0, 3, 0, 0, 0, 3, 3, 0,
+        0, 3, 0, 3, 0, 0, 0, 3, 3, 0,
+        0, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      id = 2,
+      name = "walls",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 2, 2, 2, 2, 0, 0, 0,
+        0, 0, 0, 2, 0, 0, 2, 0, 0, 0,
+        0, 0, 0, 2, 0, 0, 2, 0, 0, 0,
+        0, 0, 0, 2, 2, 2, 2, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        2, 9, 9, 9, 2, 0, 2, 2, 2, 2,
+        7, 0, 2, 0, 6, 0, 2, 0, 0, 2,
+        7, 0, 2, 0, 6, 0, 2, 0, 0, 2,
+        7, 0, 0, 0, 6, 0, 2, 2, 2, 2,
+        2, 8, 8, 8, 2, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      id = 3,
+      name = "items",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 11, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 10, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 4, 0, 0, 0, 10, 5, 0,
+        0, 11, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    }
+  }
+}
